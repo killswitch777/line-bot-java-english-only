b9182ec9b7b59845f3920b5f787fdb3155399e87
austin0910 <Austin.cheyao.chen@gmail.com>
2017-09-09 09:29:04 +0800

    Update EchoApplication.java

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -25,6 +25,7 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import com.linecorp.bot.model.ReplyMessage;
 
 @SpringBootApplication
 @LineMessageHandler
